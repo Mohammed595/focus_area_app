b2aa9217cb371009f6db532f20b383abdf136696
mohammed595 <mohammed9ne@gmail.com>
2024-03-30 01:18:13 +0300

    push assets

---
+++ lib/features/home_screen.dart/ui/widgets/app_bar_home_screen.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:focus_area_app/core/contsans/assets_path.dart';
+import 'package:focus_area_app/core/contsans/colors.dart';
+
+class AppBarForHomeScreen extends StatelessWidget {
+  const AppBarForHomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,          
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          const Text(
+            'Focus',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: AppColors.praimryColor,
+            ),
+          ),
+          Positioned(
+            right:70,
+            child: SvgPicture.asset(AppIcons.peperAirPlaneIcon),
+          ),
+        ],
+      ),
+    );
+  }
+}
