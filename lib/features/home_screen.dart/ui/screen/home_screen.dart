1fc9e68df61727dd239054cec49d16b8e9b59626
mohammed595 <mohammed9ne@gmail.com>
2024-03-30 00:20:10 +0300

    test for first push on source tree

--- lib/features/home_screen.dart/ui/screen/home_screen.dart
+++ lib/features/home_screen.dart/ui/screen/home_screen.dart
@@ -9,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Home Screen'),
+        child: Text(' just for test on source tree'),
       ),
     );
   }
